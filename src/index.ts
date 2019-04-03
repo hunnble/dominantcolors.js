b59d2a8e151866408c2ec382bd0961443e3fab8e
hunnble <hante_hunter@163.com>
2019-04-04 01:16:35 +0800

    WIP: get image data

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,27 @@
-export default function () {
-  console.log(1)
+async function getImageData (src: string): Promise<Uint8ClampedArray> {
+  return new Promise((resolve, reject) => {
+    const image = new Image()
+    image.src = src
+
+    image.onload = () => {
+      const { width, height } = image
+      const canvas = document.createElement('canvas')
+      canvas.setAttribute('width', String(width))
+      canvas.setAttribute('width', String(height))
+      const context = canvas.getContext('2d')
+      context.drawImage(image, 0, 0, width, height)
+      const { data } = context.getImageData(0, 0, width, height)
+      resolve(data)
+    }
+
+    image.onerror = image.onabort = () => {
+      reject(new Error('Load image failed, please try again.'))
+    }
+  })
+}
+
+export default async function (image: string | ArrayBuffer) {
+  if (typeof image === 'string') {
+    image = await getImageData(image)
+  }
 }
