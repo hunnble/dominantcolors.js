df69f77a4b2f3744f4d43c7d0db05aabd6395404
hunnble <hante_hunter@163.com>
2019-04-07 00:12:35 +0800

    WIP: median cut

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,40 @@
-async function getImageData (src: string): Promise<Uint8ClampedArray> {
+async function getImageData(src: string): Promise<Uint8ClampedArray> {
   return new Promise((resolve, reject) => {
-    const image = new Image()
-    image.src = src
+    const image = new Image();
+    image.src = src;
 
     image.onload = () => {
-      const { width, height } = image
-      const canvas = document.createElement('canvas')
-      canvas.setAttribute('width', String(width))
-      canvas.setAttribute('width', String(height))
-      const context = canvas.getContext('2d')
-      context.drawImage(image, 0, 0, width, height)
-      const { data } = context.getImageData(0, 0, width, height)
-      resolve(data)
-    }
+      const { width, height } = image;
+      const canvas = document.createElement("canvas");
+      canvas.setAttribute("width", String(width));
+      canvas.setAttribute("width", String(height));
+      const context = canvas.getContext("2d");
+      context.drawImage(image, 0, 0, width, height);
+      const { data } = context.getImageData(0, 0, width, height);
+      resolve(data);
+    };
 
     image.onerror = image.onabort = () => {
-      reject(new Error('Load image failed, please try again.'))
-    }
-  })
+      reject(new Error("Load image failed, please try again."));
+    };
+  });
 }
 
 interface IOptions {
-  omitTransparentPixel: Boolean
+  omitTransparentPixel: Boolean;
 }
 
 const defaultOptions = {
   omitTransparentPixel: true
-}
+};
 
-export default async function (image: string | ArrayBuffer, options: IOptions = defaultOptions) {
-  if (typeof image === 'string') {
-    image = await getImageData(image)
+export default async function(
+  image: string | ArrayBuffer | any,
+  options: IOptions = defaultOptions
+) {
+  console.log(typeof image);
+  if (typeof image === "string") {
+    image = await getImageData(image);
+    console.log(image);
   }
 }
--- src/medianCut.ts
+++ src/medianCut.ts
@@ -3,7 +3,35 @@ import { getRGBFromData } from "./helper";
 const MIN_COLOR = 0;
 const MAX_COLOR = 255;
 
-function getColorRange(data) {
+interface IColorRange {
+  red: {
+    min: number;
+    max: number;
+  };
+  green: {
+    min: number;
+    max: number;
+  };
+  blue: {
+    min: number;
+    max: number;
+  };
+}
+
+class ColorBox {
+  private total: number;
+  private data;
+  private colorRange: IColorRange;
+  private getColorRange: Function = getColorRange;
+
+  constructor(data) {
+    this.data = data;
+    this.total = data.length / 4;
+    this.colorRange = this.getColorRange(data);
+  }
+}
+
+function getColorRange(data): IColorRange {
   const pixelCount = data.length / 4;
   let minRed: number = MIN_COLOR;
   let maxRed: number = MAX_COLOR;
@@ -24,12 +52,28 @@ function getColorRange(data) {
   }
 
   return {
-    red: [minRed, maxRed],
-    green: [minGreen, maxGreen],
-    blue: [minBlue, maxBlue]
+    red: {
+      min: minRed,
+      max: maxRed
+    },
+    green: {
+      min: minGreen,
+      max: maxGreen
+    },
+    blue: {
+      min: minBlue,
+      max: maxBlue
+    }
   };
+}
+
+function getVolume(colorRange: IColorRange) {
+  const { red, green, blue } = colorRange;
+  return (red.max - red.min) * (green.max - green.min) * (blue.max - blue.min);
 }
 
 function getCutSide() {}
 
-export default function medianCut(colors, count: number = 1) {}
+export default function medianCut(boxes, count: number = 1) {
+  while (boxes.length < count) {}
+}
